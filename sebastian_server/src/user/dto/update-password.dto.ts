a140d94d8ee1e9307956d673e317a677290bb42a
hoseong3780 <55482996+hoseong3780@users.noreply.github.com>
2026-03-31 20:15:37 +0900

    update-password API연결

---
+++ sebastian_server/src/user/dto/update-password.dto.ts
@@ -0,0 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class UpdatePasswordDto {
+  @ApiProperty({ example: 'test@test.com' })
+  @IsEmail()
+  @IsNotEmpty()
+  email: string; 
+  @ApiProperty({ example: 'newPassword123' })
+  @IsString()
+  @MinLength(6)
+  password: string;
+}
